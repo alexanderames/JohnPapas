23ce300bd03b3abe80a66046a5a58612c4b29012
alexander ames <alexanderthomasames@gmail.com>
2017-07-14 15:10:10 -0700

    bind with input

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,21 +9,23 @@ var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
         this.name = 'Alexander Ames';
-        this.image = 'favicon.ico';
-        this.color = 'red';
+        this.street = '1385 S Claiborne Ave';
+        this.city = 'Gilbert';
+        this.region = 'Southwest';
+        this.hideAddress = true;
     }
-    AppComponent.prototype.clicked = function () {
-        this.color = this.color == 'red' ? 'blue' : 'red';
+    AppComponent.prototype.addressClick = function () {
+        this.hideAddress = !this.hideAddress;
     };
-    AppComponent.prototype.colorChange = function (color) {
-        this.color = color;
+    AppComponent.prototype.regionChange = function (region) {
+        this.region = region;
     };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n  \t<h1>{{name}}</h1>\n  \t<fieldset>\n  \t\t<img [src]=\"image\"/>\n  \t</fieldset>\n  \t<label [style.color]=\"color\">Favorite Color: </label>\n  \t<button (click)=\"clicked()\">Toggle Color</button>\n  \t<select (change)=\"colorChange($event.target.value)\">\n  \t\t<option>red</option>\n  \t\t<option>blue</option>\n  \t\t<option>green</option>\n  \t</select>\n  ",
+        template: "\n  \t<h1>{{name}}</h1>\n  \t<p> {{name}} is in the {{region}} region. </p>\n  \t<br/>\n\n  \t<input [value]=\"name\" (input)=\"name=$event.target.value\"><br/>\n\n  \t<input [value]=\"name\" (keyup.enter)=\"name=$event.target.value\"\n  \t\t\t\t\t\t\t\t\t\t\t\t(blur)=\"name=$event.target.value\"><br/>\n\n  \t<button (click)=\"addressClick()\">Show/Hide Address</button>\n  \t<div [hidden]=\"hideAddress\">\n  \t\t<fieldset>\n  \t\t\t<label>Street: </label>{{street}}\n  \t\t</fieldset>\n\n  \t\t<fieldset>\n  \t\t\t<label>City: </label>{{city}}\n  \t\t</fieldset>\n\n  \t\t<fieldset>\n  \t\t\t<label>Region:</label>\n  \t\t\t<select (change)=\"regionChange($event.target.value)\">\n  \t\t\t\t<option>Northwest</option>\n  \t\t\t\t<option>Southwest</option>\n  \t\t\t\t<option>Southeast</option>\n  \t\t\t\t<option>Northeast</option>\n  \t\t\t</select>\n  \t\t</fieldset>\n  \t</div>\n  \t\n  ",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,28 +4,50 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   	<h1>{{name}}</h1>
-  	<fieldset>
-  		<img [src]="image"/>
-  	</fieldset>
-  	<label [style.color]="color">Favorite Color: </label>
-  	<button (click)="clicked()">Toggle Color</button>
-  	<select (change)="colorChange($event.target.value)">
-  		<option>red</option>
-  		<option>blue</option>
-  		<option>green</option>
-  	</select>
+  	<p> {{name}} is in the {{region}} region. </p>
+  	<br/>
+
+  	<input [value]="name" (input)="name=$event.target.value"><br/>
+
+  	<input [value]="name" (keyup.enter)="name=$event.target.value"
+  												(blur)="name=$event.target.value"><br/>
+
+  	<button (click)="addressClick()">Show/Hide Address</button>
+  	<div [hidden]="hideAddress">
+  		<fieldset>
+  			<label>Street: </label>{{street}}
+  		</fieldset>
+
+  		<fieldset>
+  			<label>City: </label>{{city}}
+  		</fieldset>
+
+  		<fieldset>
+  			<label>Region:</label>
+  			<select (change)="regionChange($event.target.value)">
+  				<option>Northwest</option>
+  				<option>Southwest</option>
+  				<option>Southeast</option>
+  				<option>Northeast</option>
+  			</select>
+  		</fieldset>
+  	</div>
+  	
   `,
 })
 export class AppComponent  {
 	name = 'Alexander Ames';
-	image = 'favicon.ico';
-	color = 'red';
+	street = '1385 S Claiborne Ave';
+	city = 'Gilbert';
+	region = 'Southwest';
+	hideAddress = true;
 
-	clicked() {
-		this.color = this.color == 'red' ? 'blue' : 'red';
+	addressClick() {
+		this.hideAddress = !this.hideAddress;
 	}
 
-	colorChange(color: string) {
-		this.color = color;
+	regionChange(region: string) {
+		this.region = region;
 	}
+
 }
